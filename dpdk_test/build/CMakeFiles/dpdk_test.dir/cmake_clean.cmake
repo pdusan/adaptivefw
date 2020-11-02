9d49b8815971d0613e5e4f1ef1d42bdbe836f8ba
Dusan Popovic <dusanp96@gmail.com>
2020-11-02 12:55:58 +0100

    test files upload

---
+++ dpdk_test/build/CMakeFiles/dpdk_test.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dpdk_test.dir/dpdk_test.c.o"
+  "dpdk_test.pdb"
+  "dpdk_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dpdk_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
